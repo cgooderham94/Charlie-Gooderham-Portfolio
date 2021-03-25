9bd6ff5996659d546ae17d39c49c99afa7537a87
Charlie Gooderham <charlie@euphoriq.co.uk>
2021-03-25 22:44:11 +0000

    Initial setup, install react router, placeholder notes inn app.js outlining content to follow.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
+import { BrowserRouter } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Content to appear on site</h1>
+
+        <ul>
+          <li>
+            Basic Nav
+            <ul>
+              <li>About</li>
+              <li>Experience</li>
+              <li>Portfolio?</li>
+              <li>Contact/CV</li>
+            </ul>
+          </li>
+          <li>Impact Hero/Header Intro</li>
+          <li>LinkedIn Profile</li>
+        </ul>
+      </div>
+    </BrowserRouter>
   );
 }
 
