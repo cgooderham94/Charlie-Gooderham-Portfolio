0b4ae7527ecd2c909b55e3d84897353a5d3bda03
Charlie Gooderham <charlie@euphoriq.co.uk>
2021-04-03 19:28:05 +0100

    Add base styling including css vars. Add basic navigation, logo and nav item styles. Amend headline component styles for mobile versatility.

--- src/components/Headline/Headline.js
+++ src/components/Headline/Headline.js
@@ -1,6 +1,6 @@
 import classes from './Headline.module.css';
 
-function Headline() {
+const Headline = () => {
     return <h1 className={classes.Headline}><span>Hi.</span><span>I'm Charlie.</span><span>Front-end developer with a curiosity for UX and a passion for crafting user-focused digital experiences.</span></h1>
 }
 
