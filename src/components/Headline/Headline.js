22d16630abb3c0ce1fb91fb31d69a3ea8e44f141
Charlie Gooderham <charlie@euphoriq.co.uk>
2021-04-03 22:40:47 +0100

    Style tweaking, add emjoi's for placeholder content.

--- src/components/Headline/Headline.js
+++ src/components/Headline/Headline.js
@@ -1,7 +1,11 @@
 import classes from './Headline.module.css';
 
 const Headline = () => {
-    return <h1 className={classes.Headline}><span>Hi.</span><span>I'm Charlie.</span><span>Front-end developer with a curiosity for UX and a passion for crafting user-focused digital experiences.</span></h1>
+    return <h1 className={classes.Headline}>
+                <span>Hi. 👋</span>
+                <span>I'm Charlie. 👨🏽‍💻</span>
+                <span>Front-end developer with a curiosity for UX and a passion for crafting user-focussed digital experiences.</span>
+            </h1>
 }
 
 export default Headline;
