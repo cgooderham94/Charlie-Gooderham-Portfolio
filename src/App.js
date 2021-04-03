57d11d0bf60e8a53003b5cce7a0959f36d17d8a6
Charlie Gooderham <charlie@euphoriq.co.uk>
2021-04-03 19:24:35 +0100

    Add skeleton GetInTouch container, import and use in App.js. Basic app root syling.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import {
   Switch
  } from 'react-router-dom';
 
+import Navigation from './components/Navigation/Navigation';
 import Landing from './containers/Landing/Landing';
 import Portfolio from './containers/Portfolio/Portfolio';
-import './App.module.css';
+import GetInTouch from './containers/GetInTouch/GetInTouch';
+import classes from './App.module.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <main className={classes.App}>
+        <Navigation></Navigation>
         
         <Switch>
           <Route exact path="/">
@@ -21,9 +24,13 @@ function App() {
           <Route exact path="/portfolio">
             <Portfolio />
           </Route>
+
+          <Route exact path="/get-in-touch">
+            <GetInTouch/>
+          </Route>
         </Switch>
 
-      </div>
+      </main>
     </BrowserRouter>
   );
 }
---
+++ src/containers/GetInTouch/GetInTouch.js
@@ -0,0 +1,8 @@
+const GetInTouch = () => {
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default GetInTouch;
